9c0150af81654f695556b5b74dc09ede70abc1b3
Romicha Parvin <romichaparvin35@gmail.com>
2026-07-04 13:34:33 +0600

    challange solved

---
+++ challange.js
@@ -0,0 +1,29 @@
+let salary = 65000;
+
+if(salary >= 50000) {
+    console.log("Bonus Approved")
+} else{
+    console.log("Bonus Not Approved")
+}
+
+//challange 2 
+
+let age = 17;
+
+if(age >= 18){
+    console.log("You are  eligible to create an account")
+} else {
+    console.log("You are not eligible to create an account")
+}
+
+//challange 3
+
+let correctPassword = "123456";
+let enteredPassword = "123456";
+
+if(correctPassword === enteredPassword){
+    console.log("Login Successfull")
+} else {
+    console.log("Login Failed")
+}
+
